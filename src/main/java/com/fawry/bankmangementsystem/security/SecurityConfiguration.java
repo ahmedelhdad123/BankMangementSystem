db71ff499fca8079473db782a308e03c980af6c7
ahmedelhdad123 <ahmedelhdad956@gmail.com>
2024-08-21 16:52:24 +0300

    version 6(Add Swagger)

--- src/main/java/com/fawry/bankmangementsystem/security/SecurityConfiguration.java
+++ src/main/java/com/fawry/bankmangementsystem/security/SecurityConfiguration.java
@@ -31,7 +31,17 @@ public class SecurityConfiguration {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
                                 "/api/auth/**",
-                                "/api/transactions/**"
+                                "/api/transactions/**",
+                                "/v2/api-docs",
+                                "/v3/api-docs",
+                                "/v3/api-docs/**",
+                                "/swagger-resources",
+                                "/swagger-resources/**",
+                                "/configuration/ui",
+                                "/configuration/security",
+                                "/swagger-ui/**",
+                                "/webjars/**",
+                                "swagger-ui.html"
                         )
                         .permitAll()
                         .anyRequest()
